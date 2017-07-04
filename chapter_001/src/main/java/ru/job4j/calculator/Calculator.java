47daa44934b4cdee146e8c959aa4f0bd3ee414a5
Mrdomax <kasian.job@gmail.com>
2017-07-04 13:55:14 +0300

    2.3. Элементарный калькулятор. [#185]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,64 @@
+package ru.job4j.calculator;
+/**
+*Class Calculator.
+*@author Semenchenko.
+*@since 01.
+*/
+public class Calculator {
+	/**
+	*@param result параметр.
+    */
+    private double result;
+    /**
+	*@param first параметр.
+    *@param second параметр.
+    */
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+    /**
+	*@return параметр result.
+    */
+	public double getResult() {
+        return this.result;
+    }
+	/**
+	*@param first параметр.
+    *@param second параметр.
+    */
+    public void substruct(double first, double second) {
+        this.result = first - second;
+    }
+    /**
+	*@return параметр result.
+    */
+	public double getResult1() {
+        return this.result;
+    }
+	/**
+	*@param first параметр.
+    *@param second параметр.
+    */
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+	/**
+	*@return параметр result.
+    */
+    public double getResult2() {
+        return this.result;
+    }
+	/**
+	*@param first параметр.
+    *@param second параметр.
+    */
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+	/**
+	*@return параметр result.
+    */
+    public double getResult3() {
+        return this.result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.calculator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+*Class CalculatorTest.
+*@author Semenchenko.
+*@since 01.
+*/
+public class CalculatorTest {
+	/**
+	*@Test
+	*/
+    public void whenAddOnePlusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+    /**
+	*@Test
+	*/
+	public void whenSubstructOneMinOneThenZer() {
+        Calculator calc = new Calculator();
+        calc.substruct(1D, 1D);
+        double result = calc.getResult1();
+        double expected = 0;
+        assertThat(result, is(expected));
+    }
+	/**
+	*@Test
+	*/
+	public void whenDivTwoOnTwoThenOne() {
+        Calculator calc = new Calculator();
+        calc.div(2D, 2D);
+        double result = calc.getResult2();
+        double expected = 1;
+        assertThat(result, is(expected));
+    }
+	/**
+	*@Test
+	*/
+	public void whenMultipleTwoOnTwoThenFor() {
+        Calculator calc = new Calculator();
+        calc.multiple(2D, 2D);
+        double result = calc.getResult3();
+        double expected = 4;
+        assertThat(result, is(expected));
+    }
+}
